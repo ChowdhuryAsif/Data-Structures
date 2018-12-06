31c1d7b57e087c839d5c9ee93983f1a44cce5bc8
Asif Ahmed Chowdhury <38984204+ChowdhuryAsif@users.noreply.github.com>
2018-12-06 19:03:25 +0600

    Array Based Implimentation of Stack

--- Stack.cpp
+++ Stack.cpp
@@ -8,15 +8,13 @@ using namespace std;
 class Stack
 {
     int st[SIZE];
-    int topPosition;
-    int backPosition = 0;
+    int topPosition = -1;
 
 public:
 
     void push(int val){
-        st[backPosition] = val;
-        backPosition++;
-        topPosition = backPosition - 1;
+        topPosition++;
+        st[topPosition] = val;
     }
 
     void pop(){
@@ -37,23 +35,23 @@ int main()
 {
     Stack st;
 
-    for(int i=1; i<=10; i++)
+    for(int i=1; i<=5; i++)
         st.push(i);
 
-    if(!st.isEmpty())
-        cout << "Not empty" << endl;
-    else
-        cout << "Empty" << endl;
 
     while(!st.isEmpty()){
         cout << st.top() << endl;
         st.pop();
     }
 
-    if(!st.isEmpty())
-        cout << "Not empty" << endl;
-    else
-        cout << "Empty" << endl;
+    for(int i=100; i<=105; i++)
+        st.push(i);
+
+
+    while(!st.isEmpty()){
+        cout << st.top() << endl;
+        st.pop();
+    }
 
     return 0;
 }
